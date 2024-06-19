edf70b102927e37dde1dd70774bc5c233dabe08b
tlmoody84 <156528348+tlmoody84@users.noreply.github.com>
2024-06-18 20:15:18 -0400

    corrected error code from the run build

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -5,10 +5,8 @@ export default function About() {
     <div className="container mx-auto px-4 py-8 bg-black-100"> {/* Container with pink background */}
       <h1 className="text-3xl font-red text-center mb-4 text-pink-700">All About Me</h1> {/* Centered heading with pink color */}
       <p className="text-black-700 leading-loose text-center">  {/* Centered paragraph with gray text */}
-        Hello! My name is Tiffany Sharpe. I am a Fullstack Developer with a passion
-        for creating beautiful and functional websites. I have been working as a
-        developer since January 2024 and I am always eager to learn and improve my
-        skills.
+        My name is Tiffany Sharpe. I am a Fullstack Developer with a strong passion
+        for creating developing websites. I am always eager and willing to learn. I work each project until it is complete and functional.
       </p>
       <h2 className="text-2xl font-bold text-center mb-4 text-purple-600">Tiffany Skills</h2> {/* Centered skills heading with purple color */}
       <ul className="list-disc pl-4 mx-auto"> {/* Centered unordered list with disc and padding */}
